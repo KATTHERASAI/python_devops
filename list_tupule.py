9f184301eeba221055ca8047241684315d94259a
KATTHERASAI <kattherasaikrishna963@gmail.com>
2026-06-11 12:10:56 +0530

    adding the file operation function

--- list_tupule.py
+++ list_tupule.py
@@ -7,6 +7,6 @@ print(list_of_s3[0])
 
 
 list_of_ec2 = ("s", "a", "i")
-# list_of_ec2.append("k")
+# list_of_ec2.append("k")  # Tuples are immutable, so this will raise an AttributeError
 
 print(list_of_ec2)
